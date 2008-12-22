f739817f3e0719d03123fa0ca921666d5e389fbb
nordri <nordri@muspells.net>
2008-12-22 17:45:57 +0100

    modified:   index.php La página inicial

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<? include_once("header.php") ?>
-<? include_once("footer.php");?>
+<? include_once("header.php"); ?>
+<div id="error">
+			<?if (strlen($_SESSION['error_login']) !=0) {
+				print($_SESSION['error_login']);
+			}?>		
+		</div>
+		<div id="content">
+			Bienvenido a Feeder, un lector de noticias social. <a href="register.php">Registrate</a>, crea tu planeta
+			y comparte noticias con tus amigos.
+		</div>
+<? include_once("footer.php"); ?>
