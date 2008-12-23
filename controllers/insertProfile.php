4f943e885754e3542bcc7932b99520cd8c774bf5
nordri <nordri@muspells.net>
2008-12-23 19:23:27 +0100

    Ajustes

--- controllers/insertProfile.php
+++ controllers/insertProfile.php
@@ -5,13 +5,13 @@
   
   $login = $_SESSION['login'];
     
-$name = "'".$_REQUEST['name']."'";
-$surname = "'".$_REQUEST['surname']."'";
-$phone = "'".$_REQUEST['phone']."'";
-$city = "'".$_REQUEST['city']."'";
-$flickr = "'".$_REQUEST['flickr']."'";
-$twitter = "'".$_REQUEST['twitter']."'";
-$web = "'".$_REQUEST['web']."'";
+  $name = "'".$_REQUEST['name']."'";
+  $surname = "'".$_REQUEST['surname']."'";
+  $phone = "'".$_REQUEST['phone']."'";
+  $city = "'".$_REQUEST['city']."'";
+  $flickr = "'".$_REQUEST['flickr']."'";
+  $twitter = "'".$_REQUEST['twitter']."'";
+  $web = "'".$_REQUEST['web']."'";
     
     $conn = new Sgbd();
     $login_id = $conn->selectFromDB("users", array("id"), array("login", "'".$login."'"));
@@ -19,7 +19,7 @@ $web = "'".$_REQUEST['web']."'";
     $res = $conn->insert2DB("profiles", array("name", "surname", "phone", "city", "flickr", "twitter", "web", "user_id"), array($name, $surname, $phone, $city, $flickr, $twitter, $web, $id));
     
     if ($res) {
-    	header("Location: ../myPlanets.php");
+    	header("Location: ../profile.php");
     } else {
     	print "Falló la inserción";
 	 }
