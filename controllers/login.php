fc37819ae0abc1d1391239d9f08662f4051d26c1
nordri <nordri@muspells.net>
2008-12-24 14:26:33 +0100

    Cookie support

--- controllers/login.php
+++ controllers/login.php
@@ -2,16 +2,17 @@
 	
 	include ("../lib/util.php");
 	include ("../lib/sgbd.php");
-	session_start();
-       
-   $login = $_REQUEST['login'];
-   $password = codificaPasswd($_REQUEST['password']);
-    
+  session_start();
+   
    $conn = new Sgbd();
+   $validator = new Utilities();
+   $login = $_REQUEST['login'];
+   $password = $validator->codificaPasswd($_REQUEST['password']);
    $result = $conn->selectFromDB("users", array("login","password"), array("login", "'".$login."'"));      
    
    if ($password == $result[0][password]) {
 		$_SESSION['flash_notice'] = "Bienvenido!";
+    setcookie("login", $login, time()+36000, "/");
 		header("Location: ../myPlanets.php");
    } else {
 		$_SESSION['flash_error'] = "Nombre de usuario o contraseña incorrectos";
