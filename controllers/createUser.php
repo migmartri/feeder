da66edaa3f925a372b158b35e1915604d676eb0e
nordri <nordri@muspells.net>
2008-12-24 11:35:04 +0100

    Login unico, no más de un mismo username

--- controllers/createUser.php
+++ controllers/createUser.php
@@ -1,6 +1,7 @@
 <?php
 	include ("../lib/util.php");
-   include ("../lib/sgbd.php");
+  include ("../lib/sgbd.php");
+  session_start();
     
     $login = "'".$_REQUEST['login']."'";
     $password = "'".codificaPasswd($_REQUEST['password'])."'";
@@ -8,8 +9,14 @@
     $fecha = "'".date("Y-n-j H:i:s")."'";
     
     $conn = new Sgbd();
-
-    $res = $conn->insert2DB("users", array("login", "password", "email", "created_at"), array($login, $password, $email, $fecha));
+    $exist = $conn->selectFromDB("users", array("login"), array("login", $login));
+    
+    if (count($exist) == 0) {
+      $res = $conn->insert2DB("users", array("login", "password", "email", "created_at"), array($login, $password, $email, $fecha));
+    } else {
+      $_SESSION['flash_error'] = "El nombre de usuario ya está en uso, intente otro.";
+      header("Location: ../login.php");
+    }
     
     if ($res) {
     	header("Location: ../myPlanets.php");
