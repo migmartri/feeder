172f7e0843521a51e84ad30b8c3b4a2c9f1e1304
nordri <nordri@muspells.net>
2008-12-24 13:00:11 +0100

    Check no empty fields; check valid url

--- controllers/insertProfile.php
+++ controllers/insertProfile.php
@@ -3,7 +3,9 @@
   include ("../lib/sgbd.php");
   session_start();
   
-  $login = $_SESSION['login'];
+  $login = "admin"; //$_SESSION['login'];
+  $conn = new Sgbd();
+  $validator = new Utilities();
     
   $name = "'".$_REQUEST['name']."'";
   $surname = "'".$_REQUEST['surname']."'";
@@ -12,15 +14,51 @@
   $flickr = "'".$_REQUEST['flickr']."'";
   $twitter = "'".$_REQUEST['twitter']."'";
   $web = "'".$_REQUEST['web']."'";
+   
+  $errors = array();
+  $valid = 1;
     
-    $conn = new Sgbd();
+  if (!$validator->isEmpty($name)) {
+    $valid = 0;
+    array_push($errors, "No puede dejar el nombre en blanco");
+  }
+  if (!$validator->isEmpty($surname)) {
+    $valid = 0;
+    array_push($errors, "No puede dejar el apellido en blanco");
+  }
+  if (!$validator->isEmpty($phone)) {
+    $valid = 0;
+    array_push($errors, "No puede dejar el teléfono en blanco");
+  }
+  if (!$validator->isEmpty($city)) {
+    $valid = 0;
+    array_push($errors, "No puede dejar la ciudad en blanco");
+  }
+  if ($validator->isEmpty($flickr) && $validator->isURL($flickr)) {
+    $valid = 0;
+    array_push($errors, "La URL de Flickr no es correcta.");
+  }
+  if ($validator->isEmpty($twitter) && $validator->isURL($twitter)) {
+    $valid = 0;
+    array_push($errors, "La URL de Twitter no es correcta.");
+  }
+  if ($validator->isEmpty($web) && $validator->isURL($web)) {
+    $valid = 0;
+    array_push($errors, "La URL de tu página web no es correcta.");
+  }
+  
+  if ($valid) {
     $login_id = $conn->selectFromDB("users", array("id"), array("login", "'".$login."'"));
     $id = "'".$login_id[0][id]."'";
     $res = $conn->insert2DB("profiles", array("name", "surname", "phone", "city", "flickr", "twitter", "web", "user_id"), array($name, $surname, $phone, $city, $flickr, $twitter, $web, $id));
-    
+      
     if ($res) {
-    	header("Location: ../profile.php");
+      header("Location: ../profile.php");
     } else {
-    	print "Falló la inserción";
-	 }
+      print "Falló la inserción";
+    }
+  } else {
+    $_SESSION['flash_error'] = implode(", ", $errors);
+      header("Location: ../createProfile.php");
+  }
 ?>
