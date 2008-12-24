35fd27840df8d1a0b1ef8c179e282a9488c3498c
nordri <nordri@muspells.net>
2008-12-24 14:27:07 +0100

    cookie support

--- myPlanets.php
+++ myPlanets.php
@@ -1,7 +1,14 @@
-<? include_once("templates/header.php"); ?>
+<?
+$login = $_COOKIE['login'];
+if (count($login) == 0) {
+  $_SESSION['flash_error'] = "Sesión inválida";
+  header("Location: index.php");
+}
+include_once("templates/header.php");
+?>
 
 <div id="div_login">
-  nickel
+  <? print "<a href=\"profile.php\">".$login."</a>"; ?>
 </div>
 <div id="div_planets">
   Aun no tienes ningún planeta. <a href="newPlanet.php">Crear el primero.</a>
