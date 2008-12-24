8d34804a15ff9d948e2d50703ff42f135af2514f
nordri <nordri@muspells.net>
2008-12-24 14:27:33 +0100

    Testing php file

--- showProfile.php
+++ showProfile.php
@@ -1,13 +1,5 @@
-	<?php
-  include_once("lib/util.php");
-  
-  $f= "nordri.blogsome.com";
-  
-  $validator = new Utilities();
-  
-  $res = $validator->isURL($f);
-  print_r ($res);
-  ?>
-  
-		
-    
+<?php
+// set the cookies
+  echo "Tu nombre es: ".$_COOKIE['login']."<br/>";
+  echo "Tu nombre es: ".$_COOKIE['PHPSESSID']."<br/>";
+?>
