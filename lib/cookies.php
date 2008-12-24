32bfc323b1e161fc13349f529e1aa30562d65db0
nordri <nordri@muspells.net>
2008-12-24 14:26:51 +0100

    Obsolete

--- lib/cookies.php
+++ lib/cookies.php
@@ -1,14 +1,16 @@
 <?php
-
-  function createCookie() {
+class Cookies {
+  function createCookie($cookie_name, $cookie_value, $cookie_time) {
+    setcookie($cookie_name, $cookie_value, $cookie_time);
+	}
+	
+	function readCookie($cookie_name) {
+    return $_COOKIE[$cookie_name];
 	
 	}
 	
-	function readCookie() {
-	
+	function deleteCookie($cookie_name) {
+    setcookie($cookie_name, "", time()-3600);
 	}
-	
-	function deleteCookie() {
-	
-	}
+}
 ?>
