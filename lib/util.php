2949a250abd2b3a772b6de383027ffea49f0833d
nordri <nordri@muspells.net>
2008-12-24 13:01:36 +0100

    isEmpty function; isURL function

--- lib/util.php
+++ lib/util.php
@@ -1,5 +1,17 @@
 <?php
-function codificaPasswd($pass) {
-  return sha1($pass);
+
+class Utilities {
+  
+  function codificaPasswd($pass) {
+    return sha1($pass);
+  }
+  
+  function isEmpty($field) {
+    return count($field);
+  }
+  
+  function isURL($url) {
+    return preg_match('/^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/' ,$url);
+  }
 }
 ?>
