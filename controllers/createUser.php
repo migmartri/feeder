337b8256b7fa6adc1e839866baf59e140d8095b4
nordri <nordri@muspells.net>
2008-12-24 12:01:40 +0100

    Check no empty fields

--- controllers/createUser.php
+++ controllers/createUser.php
@@ -3,25 +3,46 @@
   include ("../lib/sgbd.php");
   session_start();
     
+    $validator = new Utilities();
+    $con = new Sgbd();
+    
     $login = "'".$_REQUEST['login']."'";
-    $password = "'".codificaPasswd($_REQUEST['password'])."'";
+    $password = "'".$validator->codificaPasswd($_REQUEST['password'])."'";
     $email = "'".$_REQUEST['email']."'";
     $fecha = "'".date("Y-n-j H:i:s")."'";
     
-    $conn = new Sgbd();
-    $exist = $conn->selectFromDB("users", array("login"), array("login", $login));
+    $errors = array();
+    $valid = 1;
     
-    if (count($exist) == 0) {
-      $res = $conn->insert2DB("users", array("login", "password", "email", "created_at"), array($login, $password, $email, $fecha));
-    } else {
-      $_SESSION['flash_error'] = "El nombre de usuario ya está en uso, intente otro.";
-      header("Location: ../login.php");
+    if ($validator->isEmpty($login)) {
+      $valid = 0;
+      array_push($errors, "No puede dejar el login en blanco");
+    }
+    if ($validator->isEmpty($password)) {
+      $valid = 0;
+      array_push($errors, "No puede dejar la contraseña en blanco");
+    }
+    if ($validator->isEmpty($email)) {
+      $valid = 0;
+      array_push($errors, "No puede dejar el email en blanco");
     }
     
-    if ($res) {
-    	header("Location: ../myPlanets.php");
+    if($valid) {
+      $exist = $conn->selectFromDB("users", array("login"), array("login", $login));
+      if (count($exist) == 0) {
+        $res = $conn->insert2DB("users", array("login", "password", "email", "created_at"), array($login, $password, $email, $fecha));
+      } else {
+        $_SESSION['flash_error'] = "El nombre de usuario ya está en uso, intente otro.";
+        header("Location: ../login.php");
+      }
+      if ($res) {
+        header("Location: ../myPlanets.php");
+      } else {
+        echo "El vector con indice $c tiene el valor $v<br/>";
+      }
     } else {
-   		echo "El vector con indice $c tiene el valor $v<br/>";
-	 }
+      $_SESSION['flash_error'] = implode(", ", $errors);
+      header("Location: ../register.php");
+    }
 
 ?>
