cd105542f630a4e13c7d0d139733e81f16637269
nordri <nordri@muspells.net>
2009-01-08 20:26:10 +0100

    Reescritura futura

--- lib/cookies.php
+++ lib/cookies.php
@@ -1,16 +1,5 @@
 <?php
 class Cookies {
-  function createCookie($cookie_name, $cookie_value, $cookie_time) {
-    setcookie($cookie_name, $cookie_value, $cookie_time);
-	}
-	
-	function readCookie($cookie_name) {
-    return $_COOKIE[$cookie_name];
-	
-	}
-	
-	function deleteCookie($cookie_name) {
-    setcookie($cookie_name, "", time()-3600);
-	}
+  // nordri lo reescribe con 4 chorradas que se puedas hacer con cookies
 }
 ?>
