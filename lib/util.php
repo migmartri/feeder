fc5cf780a9129195f8fea0a955ed5b99a709510b
nickel <juan@flowersinspace.com>
2009-01-10 23:03:42 +0100

    Giving style to myPlanets

--- lib/util.php
+++ lib/util.php
@@ -126,5 +126,22 @@ class Utilities {
       array_push($GLOBALS["errors"], $msg);
     }
   }
+	// Original PHP code by Chirp Internet: www.chirp.com.au
+  // Please acknowledge use of this code by including this header.
+
+  function truncate($string, $limit, $break=" ", $pad="...")
+  {
+    // return with no change if string is shorter than $limit
+
+    if(strlen($string) <= $limit) return $string;
+
+    $string = substr($string, 0, $limit);
+    if(false !== ($breakpoint = strrpos($string, $break))) {
+      $string = substr($string, 0, $breakpoint);
+    }
+
+    return $string . $pad;
+  }
+
 }
 ?>
--- templates/planetIndex.php
+++ templates/planetIndex.php
@@ -1,21 +1,17 @@
 <div id="planet_<?=$planet['id']?>" class="planet">
-
-  <div class='options'>
-  <a href="/subscriptions.php?planet_id=<?=$planet['id']?>">Suscripciones</a> |
-    <a href="/controllers/destroyPlanet.php?id=<?=$planet['id']?>" onclick="if (!confirm('¿Estás seguro?')) {return false;}">Borrar</a>
+  <div class='planet_name'>
+		<img src="/images/planet.png" />
+    <a href="/planet.php?id=<?=$planet['id']?>"> <?=$planet['name']?></a> 
   </div>
-
-  <div class='planet_info'>
-    <div class='planet_name'>
-      <a href="/planet.php?id=<?=$planet['id']?>"> <?=$planet['name']?></a> 
-    </div>
-    <div class='planet_description'>
-      <p>
-        <?= strip_tags($planet['description']) ?>
-      </p>
-    </div>
+	<div class='planet_description'>
+		<p>
+			<?= $util->truncate(strip_tags($planet['description']), 100) ?>
+		</p>
+	</div>
+  <div class='planet_options'>
+		<img src="/images/link.png" />
+	  <a href="/subscriptions.php?planet_id=<?=$planet['id']?>">Suscripciones</a>
+		<img src="/images/delete.png" />
+    <a href="/controllers/destroyPlanet.php?id=<?=$planet['id']?>" onclick="if (!confirm('¿Estás seguro?')) {return false;}">Eliminar</a>
   </div>
-
-
-
 </div>
