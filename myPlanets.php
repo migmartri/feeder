fc7a184d69c543a58b59028632e9b2219319db00
nickel <juan@flowersinspace.com>
2009-01-10 20:41:32 +0100

    Giving style to planets

--- myPlanets.php
+++ myPlanets.php
@@ -9,14 +9,24 @@
 ?>
 
 <div id="div_planets">
-<? if(count($planets) == 0){
-  print("Aun no tienes ningún planeta. <a href=\"newPlanet.php\">Crear el primero!</a>");}
-else
-  print("<a href=\"newPlanet.php\">Nuevo Planeta</a>");
-  foreach($planets as $planet){
-    include($_SERVER["DOCUMENT_ROOT"]."/templates/planetIndex.php"); 
-  }
-?>
+	<? if(count($planets) == 0){ ?>
+		<div id="first_planet">
+			<span class="big">Aun no tienes ningún planeta ;-(</span><br/>
+			<span class="biggest">
+				<a href="newPlanet.php">Crea el primero!</a>
+			</span><br/>
+			<a href="newPlanet.php"><img src="/images/add_big.png" /></a>
+		</div>
+	<? } else { ?>
+		<div id="new_planet">
+			<a href="newPlanet.php"><img src="/images/add.png" /></a>
+			<a href="newPlanet.php">Nuevo Planeta</a>
+		</div>
+	<? } ?>
+	<? foreach($planets as $planet){
+			include($_SERVER["DOCUMENT_ROOT"]."/templates/planetIndex.php"); 
+		 }		
+	?>	
 </div>
 
 <? include_once ($_SERVER["DOCUMENT_ROOT"]."/templates/footer.php"); ?>
--- newPlanet.php
+++ newPlanet.php
@@ -6,18 +6,24 @@
 ?>
 
 <form action="/controllers/createPlanet.php" method="post" onsubmit="return validatesPlanet()">
-  <fieldset>
-    <legend>Nuevo Planeta</legend>
-    <div id="div_name">
-      <label id="label_name" for="name">Nombre del planeta</label>
-      <input id="name" name="name" type="text" value="<?=$util->formValue('name')?>"/>
-    </div>
-    <div id="div_description">
-      <label id="label_description" for="description">Descripción del planeta</label>
-      <textarea id="description" name="description" rows="3" cols="30"/><?=$util->formValue('description')?></textarea>
-    </div>
-
-  <button id="submit" >Enviar</button>
+	<div id="div_nuevo_planeta" class="form form-planet">
+		<fieldset>
+			<legend>Nuevo Planeta</legend>
+			<div class="fields">
+				<div id="div_name">
+					<label id="label_name" for="name">Nombre del planeta</label><br/>
+					<input id="name" name="name" type="text" value="<?=$util->formValue('name')?>"/>
+				</div>
+				<div id="div_description">
+					<label id="label_description" for="description">Descripción del planeta</label><br/>
+					<textarea id="description" name="description" rows="3" cols="30"/><?=$util->formValue('description')?></textarea>
+				</div>
+			</div>
+		</fieldset>
+		<div id="div_submit">
+			<button id="submit" >Enviar</button>
+		</div>
+	</div>
 </form>
 
 <? include_once ($_SERVER["DOCUMENT_ROOT"]."/templates/footer.php"); ?>
