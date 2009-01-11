a799bc701d430f2fd861cbff4e23ca5f8c488414
N3uro5i5 <n3uro5i5@gmail.com>
2009-01-11 15:22:24 +0100

    content;encoded

--- rssPlanet.php
+++ rssPlanet.php
@@ -1,7 +1,7 @@
 <?
-include ($_SERVER['DOCUMENT_ROOT']."/lib/sgbd.php");
+  include ($_SERVER['DOCUMENT_ROOT']."/lib/sgbd.php");
   $conn = new Sgbd();
-  $planet = $conn->selectFromDB("first", "planets", array("*"), array("id" => $_GET['id']));
+  $planet = $conn->selectFromDB("first", "planets", array("*"), array("id" => $_GET['id'])) ;
 
   if(!$planet){
     $_SESSION['flash_error'] = "No existe el planeta al que quiere acceder o no tiene permisos para verlo.";
@@ -10,27 +10,29 @@ include ($_SERVER['DOCUMENT_ROOT']."/lib/sgbd.php");
 
   $posts = $conn->findBySql("SELECT * FROM posts WHERE feed_id IN (SELECT feed_id FROM feeds_planets WHERE planet_id =".$planet['id'].") ORDER BY published_at DESC LIMIT 10"); 
 
-header('Content-type: text/xml; charset="UTF-8"', true);
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo
-'<rss version="0.92">
-    <channel>
-         <docs>http://github.com/N3uro5i5/feeder/tree/master</docs>
-         <title>Planeta'.$planet['name'].'</title>
-         <link>'.$_SERVER['SERVER_NAME'].'/planet?id='.$planet['id'].'</link>
-         <description>'.$planet['description'].'</description>
-         <language>es</language>
-         <managingEditor></managingEditor>
-         <webMaster>admin@feeder.com</webMaster>
-';
-foreach($posts as $post) {
-  echo "<item>" ;
-  echo "<title><![CDATA[".$post['title']."]]></title>" ;
-  echo "<link>".$post['url']."</link>";
-  echo "<description><![CDATA[".$post['content']."]]></description>";
-  echo "</item>";
-}
-echo "</channel>";
-echo "</rss>";   
+  header('Content-type: text/xml; charset="UTF-8"', true);
+  echo '<?xml version="1.0" encoding="UTF-8"?>';
+  echo
+  '<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+      <channel>
+           <docs>http://github.com/N3uro5i5/feeder/tree/master</docs>
+           <title>Planeta'.$planet['name'].'</title>
+           <link>'.$_SERVER['SERVER_NAME'].'/planet?id='.$planet['id'].'</link>
+           <description>'.$planet['description'].'</description>
+           <language>es</language>
+           <webMaster>admin@feeder.com</webMaster>
+  ';
+  foreach($posts as $post) {
+    $description=substr($post['content'],0,300)."...";
+    echo "<item>" ;
+    echo "<title><![CDATA[".$post['title']."]]></title>" ;
+    /* echo "<pubDate>".$post["created_at"]."</pubDate>"; */
+    echo "<link>".$post['url']."</link>";
+    echo "<description><![CDATA[".$description."]]></description>";
+    echo "<content:encoded><![CDATA[".$post['content']."]]></content:encoded>";
+    echo "</item>";
+  }
+  echo "</channel>";
+  echo "</rss>";   
 ?>
 
