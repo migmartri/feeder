cfcf33cd9c19543a2394f4abaf7b1be09b7bbf25
nordri <nordri@muspells.net>
2009-01-11 19:18:10 +0100

    Documentacion de la aplicacion

--- planets.php
+++ planets.php
@@ -1,21 +1,40 @@
 <?php
+  include ($_SERVER['DOCUMENT_ROOT']."/lib/pagination.php");
   include ($_SERVER['DOCUMENT_ROOT']."/templates/header.php");
   
   $conn = new Sgbd();
-  $planets = $conn->findBySql("select p.name, u.login, p.created_at from users u, planets p where u.id = p.user_id order by p.created_at");
+  $planets = $conn->findBySql("select p.name, u.login, p.created_at, p.id from users u, planets p where u.id = p.user_id order by p.created_at");
   
-  
+     //Página actual
+    if(isset($_GET['page'])){
+      $current_page = $_GET['page'];
+    }else{
+      $current_page = 1; 
+    }
+    //Num total de elementos
+    $num_planets = $conn->countFromDB("planets", array("name"), array());
+    
+    //Paginamos
+    $pagination = new Pagination($num_planets[0]["num"], 10, $current_page, "select count(*) as num from users u, planets p where u.id = p.user_id");
+
+    //Elementos de esta página
+    $nplanets = $pagination->getElements();
+    //Números de página
+    $pagination_links = $pagination->paginationLinks();
+    print_r($num_planets);
+    echo($pagination_links);
 ?>
   <div id="planets_list">
   <table border="1" align="center">
     <tr>
-      <th>Usuario</th>
       <th>Planeta</th>
+      <th>Usuario</th>
       <th>Creado</th>
     </tr>
-    <?foreach($planets as $row) {
+    <?    
+    foreach($planets as $row) {
         print "<tr>";
-          print "<td>".$row[0]."</td>";
+          print "<td><a href=\"planet.php?id=".$row[3]."\">".$row[0]."</a></td>";
           print "<td>".$row[1]."</td>";
           print "<td>".$row[2]."</td>";
         print "</tr>";
@@ -24,5 +43,6 @@
     </tr>
   </table>
 <?
+  echo($pagination_links);
   include ($_SERVER['DOCUMENT_ROOT']."/templates/footer.php");
 ?>
