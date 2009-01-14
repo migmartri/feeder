b4871047f57b999dc34b9f7c8e5dee2514102fa7
nickel <juan@flowersinspace.com>
2009-01-14 17:17:44 +0100

    Update footer

--- templates/footer.php
+++ templates/footer.php
@@ -3,6 +3,19 @@
       <div id="footer">
 				<p>GPL license | Si eres legal, comparte</p>
 				<p><a href="help/help.html" title="Ayuda y documentación" >Documentación</a> | <a href="http://github.com/N3uro5i5/feeder/tree/master" title="Código fuente de la aplicación">Código</a> | <a href="http://groups.google.com/group/feeder-abd" title="Lista de correos de la aplicación">Lista de correos</a></p>
+				<p>
+					<a href="http://validator.w3.org/check?uri=referer"><img
+        src="http://www.w3.org/Icons/valid-xhtml10"
+        alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+	
+    			<a href="http://jigsaw.w3.org/css-validator/">
+        		<img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="¡CSS Válido!" />
+					</a>
+
+				</p>
+				
       </div>
     </div> <!-- End of container -->
   </body>
