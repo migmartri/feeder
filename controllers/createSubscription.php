ab78bcb89840a0b802de6e2fdc4a4f1a7d2deb1f
N3uro5i5 <n3uro5i5@gmail.com>
2009-01-14 16:08:27 +0100

    Suscripciones repetidas

--- controllers/createSubscription.php
+++ controllers/createSubscription.php
@@ -15,9 +15,15 @@
   $name = $util->validatesFeed($url, "Feed inválido");
 
   if(count($errors) == 0) {
-    checkAndCreateFeed($name, $url, $planet_id);
-    $_SESSION['flash_notice'] = "Suscripción creada!";
-    header("Location: ../planet.php?id=$planet_id");
+    $ok = checkAndCreateFeed($name, $url, $planet_id);
+    if(!$ok){
+      $_SESSION['form_values'] = $_REQUEST;
+      $_SESSION['flash_error'] = "Este feed ya lo tienes asignado";
+      header("Location: ../newSubscription.php");
+    }else{
+      $_SESSION['flash_notice'] = "Suscripción creada!";
+      header("Location: ../planet.php?id=$planet_id");
+    }
   }else{
     $_SESSION['form_values'] = $_REQUEST;
     $_SESSION['flash_error'] = implode("<br/>", $errors);
@@ -32,6 +38,12 @@
     /*Comprobamos si existe el feed en la base de datos, si no es así, lo creamos*/
     if($feed) {
       $feed_id = $feed['id'];
+      //Comprobamos si ya estamos asignados a éste
+      $count = $conn->countFromDB("feeds_planets", array('*'), array("feed_id" => $feed_id, "planet_id" => $planet_id));
+      if($count > 0){
+        $_SESSION['flash_error'] = "Ya tienes asignado ese feed";
+        return false;
+      }
     }else{
       $feed_id = $conn->insert2DB("feeds", array("name" => $name, "url" => $url));
       //Nos traemos sus feeds y los guardamos
@@ -44,5 +56,6 @@
     $conn->incrDecrFromDb("+1", "feeds", "subscriptions_count", array("id" => $feed_id));
     //Actualizamos el número de feeds en el planeta
     $conn->incrDecrFromDb("+1", "planets", "feeds_count", array("id" => $planet_id));
+    return true;
   }
 ?>
