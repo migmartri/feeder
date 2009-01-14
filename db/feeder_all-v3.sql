b2d5bc7e9538399e03fdfcbca5be71be5c4b7c34
Miguel Angel Martinez <n3uro5i5@gmail.com>
2009-01-14 12:43:08 +0100

    Feeder sql diagram v2

--- db/feeder_all-v3.sql
+++ db/feeder_all-v3.sql
@@ -8,6 +8,8 @@ USE `feeder`;
 -- -----------------------------------------------------
 -- Table `feeder`.`users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `feeder`.`users` ;
+
 CREATE  TABLE IF NOT EXISTS `feeder`.`users` (
   `id` SMALLINT(6) NOT NULL AUTO_INCREMENT ,
   `login` VARCHAR(255) NOT NULL ,
@@ -23,6 +25,8 @@ COLLATE = utf8_spanish_ci;
 -- -----------------------------------------------------
 -- Table `feeder`.`feeds`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `feeder`.`feeds` ;
+
 CREATE  TABLE IF NOT EXISTS `feeder`.`feeds` (
   `id` SMALLINT(11) NOT NULL AUTO_INCREMENT ,
   `url` VARCHAR(255) NOT NULL ,
@@ -38,6 +42,8 @@ COLLATE = utf8_spanish_ci;
 -- -----------------------------------------------------
 -- Table `feeder`.`posts`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `feeder`.`posts` ;
+
 CREATE  TABLE IF NOT EXISTS `feeder`.`posts` (
   `id` SMALLINT(6) NOT NULL AUTO_INCREMENT ,
   `feed_id` SMALLINT(6) NOT NULL ,
@@ -47,7 +53,6 @@ CREATE  TABLE IF NOT EXISTS `feeder`.`posts` (
   `url` VARCHAR(255) NOT NULL ,
   `description` TEXT NULL ,
   PRIMARY KEY (`id`) ,
-  INDEX `feed_i` (`feed_id` ASC) ,
   CONSTRAINT `feed_i`
     FOREIGN KEY (`feed_id` )
     REFERENCES `feeder`.`feeds` (`id` )
@@ -57,17 +62,19 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
+CREATE INDEX `feed_i` ON `feeder`.`posts` (`feed_id` ASC) ;
+
 
 -- -----------------------------------------------------
 -- Table `feeder`.`favourites`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `feeder`.`favourites` ;
+
 CREATE  TABLE IF NOT EXISTS `feeder`.`favourites` (
   `post_id` INT(11) NOT NULL ,
   `created_at` TIMESTAMP NOT NULL ,
   `user_id` SMALLINT(6) NOT NULL ,
   PRIMARY KEY (`post_id`, `user_id`) ,
-  INDEX `post_id` (`post_id` ASC) ,
-  INDEX `user_id` (`user_id` ASC) ,
   CONSTRAINT `post_id`
     FOREIGN KEY (`post_id` )
     REFERENCES `feeder`.`posts` (`id` )
@@ -82,10 +89,16 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
+CREATE INDEX `post_id` ON `feeder`.`favourites` (`post_id` ASC) ;
+
+CREATE INDEX `user_id` ON `feeder`.`favourites` (`user_id` ASC) ;
+
 
 -- -----------------------------------------------------
 -- Table `feeder`.`planets`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `feeder`.`planets` ;
+
 CREATE  TABLE IF NOT EXISTS `feeder`.`planets` (
   `id` SMALLINT(6) NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(255) NOT NULL ,
@@ -94,7 +107,6 @@ CREATE  TABLE IF NOT EXISTS `feeder`.`planets` (
   `created_at` TIMESTAMP NOT NULL ,
   `feeds_count` INT(11) NOT NULL DEFAULT 0 ,
   PRIMARY KEY (`id`) ,
-  INDEX `user_id` (`user_id` ASC) ,
   CONSTRAINT `user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `feeder`.`users` (`id` )
@@ -104,16 +116,18 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
+CREATE INDEX `user_id` ON `feeder`.`planets` (`user_id` ASC) ;
+
 
 -- -----------------------------------------------------
 -- Table `feeder`.`feeds_planets`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `feeder`.`feeds_planets` ;
+
 CREATE  TABLE IF NOT EXISTS `feeder`.`feeds_planets` (
   `planet_id` SMALLINT(6) NOT NULL ,
   `feed_id` SMALLINT(6) NOT NULL ,
   PRIMARY KEY (`planet_id`, `feed_id`) ,
-  INDEX `planet_id` (`planet_id` ASC) ,
-  INDEX `feed_id` (`feed_id` ASC) ,
   CONSTRAINT `planet_id`
     FOREIGN KEY (`planet_id` )
     REFERENCES `feeder`.`planets` (`id` )
@@ -128,10 +142,16 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
+CREATE INDEX `planet_id` ON `feeder`.`feeds_planets` (`planet_id` ASC) ;
+
+CREATE INDEX `feed_id` ON `feeder`.`feeds_planets` (`feed_id` ASC) ;
+
 
 -- -----------------------------------------------------
 -- Table `feeder`.`profiles`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `feeder`.`profiles` ;
+
 CREATE  TABLE IF NOT EXISTS `feeder`.`profiles` (
   `id` SMALLINT(6) NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(255) NULL ,
@@ -143,7 +163,6 @@ CREATE  TABLE IF NOT EXISTS `feeder`.`profiles` (
   `web` VARCHAR(255) NULL ,
   `user_id` SMALLINT(6) NOT NULL ,
   PRIMARY KEY (`id`) ,
-  INDEX `users_id` (`user_id` ASC) ,
   CONSTRAINT `users_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `feeder`.`users` (`id` )
@@ -152,6 +171,8 @@ CREATE  TABLE IF NOT EXISTS `feeder`.`profiles` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
+
+CREATE INDEX `users_id` ON `feeder`.`profiles` (`user_id` ASC) ;
 
 
 
